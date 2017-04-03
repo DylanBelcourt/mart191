6dfff1f9502f6bb306dfdb6540943bae7990b875
Dylan Belcourt <dbelcourt96@gmail.com>
2017-04-03 16:27:05 -0600

    color fix

--- HW8/sketch.js
+++ HW8/sketch.js
@@ -7,14 +7,14 @@ var fall = 3;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  background(117, 252, 255);
+  background(117, 251, 255);
   for (var i = 0; i < 8; i++) {
     cones.push( new Icecream((i + 1) * 150, 100));
   }
 }
 
 function draw() {
-  background(117, 252, 255, 15);
+  background(117, 251, 255, 15);
 
 
   for (var i = 0; i < cones.length; i++) {
